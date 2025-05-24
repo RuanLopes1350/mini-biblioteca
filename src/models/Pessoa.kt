2da251e4b2ddff743f7d401253e65657621201c1
Silvio Huan Matheus Grein Cristo Ribeiro <silviohuan@gmail.com>
2025-05-23 22:02:08 -0400

    first commit

---
+++ src/models/Pessoa.kt
@@ -0,0 +1,4 @@
+package models
+
+abstract class Pessoa (val nome: String, val CPF: String,){
+}
---
+++ src/models/Usuario.kt
@@ -0,0 +1,5 @@
+package models
+import java.util.UUID
+class Usuario (val nome: String, val cpf: String, val telefone: String, val endereco: String){
+
+}
